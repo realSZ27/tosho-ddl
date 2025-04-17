f72d3ec742a7440627091eb712a2493884e7dae0
realSZ27 <karstenandmicaiah@gmail.com>
2025-04-16 21:05:27 -0500

    Support amd64 and dockerhub

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import java.time.Instant
+
 plugins {
     id("java")
     id("org.springframework.boot") version "3.4.4"
@@ -26,14 +28,19 @@ dependencies {
 }
 
 jib {
-    to {
-        image = "ghcr.io/realsz27/tosho-ddl"
-        auth {
-            username = System.getenv("GITHUB_ACTOR")
-            password = System.getenv("GITHUB_TOKEN")
-        }
-    }
     from {
         image = "eclipse-temurin:21-jre-alpine"
+    }
+
+    container {
+        labels = mapOf(
+            "maintainer" to "SZ27 (https://github.com/realSZ27)",
+            "org.opencontainers.image.title" to "ToshoDDL",
+            "org.opencontainers.image.version" to project.version.toString(),
+            "org.opencontainers.image.created" to Instant.now().toString(),
+            "org.opencontainers.image.url" to "https://github.com/realSZ27/tosho-ddl",
+            "org.opencontainers.image.source" to "https://github.com/realSZ27/tosho-ddl",
+            "org.opencontainers.image.licenses" to "GPLv3"
+        )
     }
 }
