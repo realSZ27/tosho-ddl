a17e239336bf46217fae99cb2189e856268f2225
real_SZ27 <karstenandmicaiah@gmail.com>
2026-03-28 09:50:05 -0500

    add disabling sources

--- src/main/kotlin/dev/ddlproxy/AppConfig.kt
+++ src/main/kotlin/dev/ddlproxy/AppConfig.kt
@@ -4,29 +4,68 @@ import dev.ddlproxy.model.DownloadSource
 import dev.ddlproxy.sources.AnimeTosho.AnimeToshoSource
 import dev.ddlproxy.sources.TokyoInsider.TokyoInsiderSource
 import io.ktor.client.HttpClient
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
+import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.client.RestTemplate
 import tools.jackson.databind.ObjectMapper
 
 @Configuration
+@EnableConfigurationProperties(AppConfig.AppProperties::class)
 class AppConfig {
+
+    @ConfigurationProperties(prefix = "app")
+    data class AppProperties(
+        val sources: Map<Source, Map<String, Any>> = emptyMap()
+    )
 
     @Bean
     fun restTemplate() = RestTemplate()
 
     @Bean
-    fun httpClient() = HttpClient { 
+    fun httpClient() = HttpClient {
         expectSuccess = true
     }
 
-    @Bean 
+    @Bean
     fun objectMapper() = ObjectMapper()
 
     @Bean
-    fun downloadSources(client: HttpClient, objectMapper: ObjectMapper): List<DownloadSource> = listOf(
-        AnimeToshoSource(client, objectMapper),
-        TokyoInsiderSource(client),
+    fun downloadSources(
+        props: AppProperties,
+        sources: List<DownloadSource>
+    ): List<DownloadSource> {
+        return sources
+    }
+
+    @Bean
+    @ConditionalOnProperty(
+        prefix = "app.sources.animeTosho",
+        name = ["enabled"],
+        havingValue = "true",
+        matchIfMissing = true
+    )
+    fun animeToshoSource(
+        client: HttpClient,
+        objectMapper: ObjectMapper,
+    ) = AnimeToshoSource(
+        client,
+        objectMapper,
+    )
+
+    @Bean
+    @ConditionalOnProperty(
+        prefix = "app.sources.tokyoInsider",
+        name = ["enabled"],
+        havingValue = "true",
+        matchIfMissing = true
+    )
+    fun tokyoInsiderSource(
+        client: HttpClient,
+    ) = TokyoInsiderSource(
+        client,
     )
 
     enum class Source {
